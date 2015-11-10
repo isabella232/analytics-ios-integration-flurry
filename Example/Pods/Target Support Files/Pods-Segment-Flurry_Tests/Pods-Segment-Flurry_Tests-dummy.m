3e494d5bd4bd90b67a67fefef158b268d121666c
Prateek Srivastava <f2prateek@gmail.com>
2015-11-10 09:35:52 -0800

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-Segment-Flurry_Tests/Pods-Segment-Flurry_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Segment_Flurry_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Segment_Flurry_Tests
+@end
---
+++ Example/Pods/Target Support Files/Segment-Flurry/Segment-Flurry-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "SEGFlurryIntegration.h"
+#import "SEGFlurryIntegrationFactory.h"
+
+FOUNDATION_EXPORT double Segment_FlurryVersionNumber;
+FOUNDATION_EXPORT const unsigned char Segment_FlurryVersionString[];
+
