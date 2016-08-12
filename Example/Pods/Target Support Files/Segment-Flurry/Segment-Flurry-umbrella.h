03b681fdae6003bc65f2da6685afb9f679c237f0
Prateek Srivastava <f2prateek@gmail.com>
2016-08-12 12:59:05 -0600

    update pod

--- Example/Pods/Target Support Files/Segment-Flurry/Segment-Flurry-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "SEGFlurryIntegration.h"
-#import "SEGFlurryIntegrationFactory.h"
-
-FOUNDATION_EXPORT double Segment_FlurryVersionNumber;
-FOUNDATION_EXPORT const unsigned char Segment_FlurryVersionString[];
-
